c2a4344831838a13f303bc0db6490f29085a4049
Joan Nassr Conte <138223300+joannc07@users.noreply.github.com>
2024-07-30 20:12:54 -0300

    Update cond-palindromo.py

--- cond-palindromo.py
+++ cond-palindromo.py
@@ -1,6 +1,6 @@
 texto = input("Coloque uma palavra pare verificar se é considerada palíndroma : ")
 
-# De acordo com que eu pesquisei, a expressão "::" significa um slicing, ou seja, é possível tirar uma substring de uma string. Logo, a expressão "[::-1]" irá inverter string.
+# De acordo com que eu pesquisei, a expressão "::" significa um slicing, ou seja, é possível tirar uma substring de uma string. Logo, a expressão "[::-1]" irá inverter a string.
 if texto == texto[::-1]:
     
     print("É um palíndromo!")
